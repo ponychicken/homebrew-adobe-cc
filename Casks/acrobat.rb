c8192624c62fc02399a64a61618d7a5093e13ae3
Leo Koppelkamm <hello@leo-koppelkamm.de>
2014-12-07 23:46:57 +0100

    Acrobat cleanup

--- Casks/acrobat.rb
+++ Casks/acrobat.rb
@@ -10,10 +10,9 @@ cask :v1 => 'acrobat' do
         'MM_TRIALS' => '1234'
       }
 
-  homepage 'http://www.adobe.com/products/indesign.html'
+  homepage 'http://www.adobe.com/products/acrobat.html'
   version '11.0.0'
   sha256 '577a00361a1c130c25d9dfe7abf9979f85c91fb1bf0cfc4aa68d021b9ff38cc0'
-  #sha256 :no_check
   pkg "#{Utils.name}/#{Utils.name} Pro Installer.pkg"
 
   caveats 'You will need to quit all Adobe applications as well as all browsers for the install to succeed!'
